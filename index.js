26a6ef1992c267a8dbd1d51ec1f4d6a637f1a30f
himjohn <john.polonia25@compscihigh.org>
2024-11-13 18:05:35 +0000

    Lab Progress So Far

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+async function requestURL() {
+    const response = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
+    const data = await response.json()
+    console.log(data["meals"][0]["strInstructions"])
+    return	data
+    }
+  
+    requestURL()
